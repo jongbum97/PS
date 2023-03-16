ad52eccfb2f682c51cb7e10f5aff895b92a666b4
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-03-16 16:34:14 +0900

    [level 0] Title: 7의 개수, Time: 1.29 ms, Memory: 75.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120912. 7의 개수/7의 개수.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int solution(int[] array) {
+        int answer = 0;
+        
+        
+        for(int i = 0 ; i < array.length ; i++){
+            while(array[i]>0){
+                if(array[i]%10==7){
+                    answer++;
+                    array[i]=array[i]/10;
+                }
+                else{
+                    array[i]=array[i]/10;
+                }
+            }
+        }
+        return answer;
+    }
+}
