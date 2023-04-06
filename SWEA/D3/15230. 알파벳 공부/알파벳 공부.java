fbffb42bcf82e93f69624ae96b90f7bb43ff3365
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-04-06 16:31:34 +0900

    [D3] Title: 알파벳 공부, Time: 169 ms, Memory: 27,156 KB -BaekjoonHub

--- SWEA/D3/15230. 알파벳 공부/알파벳 공부.java
+++ SWEA/D3/15230. 알파벳 공부/알파벳 공부.java
@@ -1,30 +1,12 @@
 import java.util.*;
 class Solution
-{
-	public static void main(String args[]) throws Exception
-	{
-		Scanner sc = new Scanner(System.in);
-		
-		int T = sc.nextInt();
-	
-		for(int tc=1; tc<=T; tc++){
-			
-			String str = sc.next();
-			int ans = 0;
-			for(int i=0; i<str.length(); i++){
-				if(str.charAt(i)==i+'a'){
-					ans++;
-				}else {
-					break;
-				}
-			}
-			
-
-			System.out.println("#"+tc+" "+ans);
-		}
-		
-		
-
-		
-	}
-}
+{public static void main(String args[]) throws Exception
+{Scanner s = new Scanner(System.in);
+int T = s.nextInt();
+for(int t=1; t<=T; t++){
+String r = s.next();
+int a = 0;
+for(int i=0; i<r.length(); i++){
+if(r.charAt(i)==i+'a'){
+a++;}else {break;}}
+System.out.println("#"+t+" "+a);}}}
