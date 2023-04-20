66b4e4fe760b513cff47fa720d36bc7c1a4940d2
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-04-20 22:56:46 +0900

    [D3] Title: 통나무 자르기, Time: 132 ms, Memory: 21,108 KB -BaekjoonHub

---
+++ SWEA/D3/14692. 통나무 자르기/통나무 자르기.java
@@ -0,0 +1,1 @@
+import java.util.*;public class Solution {public static void main(String args[]) throws Exception {Scanner s=new Scanner(System.in);int T=s.nextInt();for(int t=1;t<=T;t++){int N = s.nextInt();System.out.println("#"+t+" "+((N%2==0)?"Alice":"Bob"));}}}
