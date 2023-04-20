3f357c109b39f9a8de4d97cb6b73303915fe01a9
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-04-20 22:49:46 +0900

    [D3] Title: 두가지 빵의 딜레마, Time: 171 ms, Memory: 28,180 KB -BaekjoonHub

---
+++ SWEA/D3/5162. 두가지 빵의 딜레마/두가지 빵의 딜레마.java
@@ -0,0 +1,31 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		
+		
+		
+		for (int tc = 1; tc <= T; tc++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			int C = sc.nextInt();
+			
+			int ans = C/Math.min(A, B);
+			
+			
+			
+			
+			
+			System.out.println("#" + tc + " " +ans);
+		}
+		
+		
+		
+		
+		
+	}
+}
