ac64351442d996dbcf1c36faaf51a9140cccc8c7
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-04-23 23:43:28 +0900

    [D3] Title: 구구단2, Time: 136 ms, Memory: 20,420 KB -BaekjoonHub

---
+++ SWEA/D3/12221. 구구단2/구구단2.java
@@ -0,0 +1,25 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			if(A>9 || B>9){
+				System.out.println("#" + tc + " "+(-1));
+			}else {
+				System.out.println("#" + tc + " "+(A*B));
+			}
+			
+			
+
+			
+		}
+
+	}
+}
