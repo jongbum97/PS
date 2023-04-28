eaf504f0767b8f301491936afc25388a2cc809d1
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-04-29 00:12:39 +0900

    [D3] Title: 24시간, Time: 162 ms, Memory: 25,004 KB -BaekjoonHub

---
+++ SWEA/D3/12368. 24시간/24시간.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+		
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+			
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			
+			System.out.println("#"+tc+" "+(A+B)%24);
+		}
+	}
+}
