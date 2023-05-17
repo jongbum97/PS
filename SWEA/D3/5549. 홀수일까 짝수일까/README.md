cfe45690e540469ccc97077c9f3f098ecac38189
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-05-17 22:45:29 +0900

    [D3] Title: 홀수일까 짝수일까, Time: 186 ms, Memory: 34,380 KB -BaekjoonHub

---
+++ SWEA/D3/5549. 홀수일까 짝수일까/README.md
@@ -0,0 +1,11 @@
+# [D3] 홀수일까 짝수일까 - 5549 
+
+[문제 링크](https://swexpertacademy.com/main/code/problem/problemDetail.do?contestProbId=AWWxpEDaAVoDFAW4) 
+
+### 성능 요약
+
+메모리: 34,380 KB, 시간: 186 ms, 코드길이: 420 Bytes
+
+
+
+> 출처: SW Expert Academy, https://swexpertacademy.com/main/code/problem/problemList.do
---
+++ SWEA/D3/5549. 홀수일까 짝수일까/홀수일까 짝수일까.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+		
+		for (int tc = 1; tc <= T; tc++) {
+
+			String s = sc.next();
+			String ans = "Odd";
+			if(s.charAt(s.length()-1)%2==0){
+				ans = "Even";
+			}
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			
+			System.out.println("#"+tc+" "+ans);
+			
+			
+			
+		}
+
+	}
+}
