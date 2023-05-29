9dc70ded16a061bc4644e893911e2382d1f4ff02
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-05-29 23:46:30 +0900

    [D1] Title: 큰 놈, 작은 놈, 같은 놈, Time: 125 ms, Memory: 20,436 KB -BaekjoonHub

---
+++ SWEA/D1/2070. 큰 놈， 작은 놈， 같은 놈/큰 놈， 작은 놈， 같은 놈.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			String ans = (a>b)? ">":(a<b)? "<":"=";
+			
+			
+			
+			
+			System.out.println("#"+tc+" "+ans);
+			
+
+		}
+	}
+}
