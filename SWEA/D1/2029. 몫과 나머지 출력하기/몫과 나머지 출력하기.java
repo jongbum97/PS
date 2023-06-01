cf1cb4a563f0de6be250eb2fe62725b8dd624489
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-01 09:29:11 +0900

    [D1] Title: 몫과 나머지 출력하기, Time: 122 ms, Memory: 20,620 KB -BaekjoonHub

---
+++ SWEA/D1/2029. 몫과 나머지 출력하기/몫과 나머지 출력하기.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int T = sc.nextInt();
+
+		for (int tc = 1; tc <= T; tc++) {
+			
+			int a = sc.nextInt();
+			int b = sc.nextInt();
+			
+			
+			
+			
+			System.out.println("#"+tc+" "+a/b+" "+a%b);
+
+		}
+	}
+}
