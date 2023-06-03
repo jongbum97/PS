b4dc262ceca6327155d5a583e65cf58db91a546a
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-03 09:43:02 +0900

    [D1] Title: 거꾸로 출력해 보아요, Time: 123 ms, Memory: 20,812 KB -BaekjoonHub

---
+++ SWEA/D1/1545. 거꾸로 출력해 보아요/거꾸로 출력해 보아요.java
@@ -0,0 +1,12 @@
+import java.util.*;
+
+public class Solution {
+	public static void main(String args[]) throws Exception {
+		Scanner sc = new Scanner(System.in);
+
+		int n = sc.nextInt();
+		for(int i=n; i>=0; i--){
+			System.out.print(i+" ");
+		}
+	}
+}
