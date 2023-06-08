4128f2a2e1c86261ff863fa11254d217608dfa38
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-08 21:49:29 +0900

    [level 1] Title: 이상한 문자 만들기, Time: 0.60 ms, Memory: 78.8 MB -BaekjoonHub

---
+++ 프로그래머스/lv1/12930. 이상한 문자 만들기/이상한 문자 만들기.java
@@ -0,0 +1,25 @@
+class Solution {
+    public String solution(String s) {
+        s = s.toLowerCase();
+
+        String[] chars = s.split("");
+
+        boolean upper = true;
+        StringBuilder builder = new StringBuilder();
+
+        for (String c : chars) {
+            if (upper) {
+                builder.append(c.toUpperCase());
+            } else {
+                builder.append(c);
+            }
+
+            upper = !upper;
+            if (c.equals(" ")) {
+                upper = true;
+            }
+        }
+
+        return builder.toString();
+    }
+}
