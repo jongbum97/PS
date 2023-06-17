ecbba88dce5b63e03279ada52d95a16fcab10ff3
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-18 00:06:03 +0900

    [Bronze V] Title: 아!, Time: 204 ms, Memory: 17636 KB -BaekjoonHub

---
+++ 백준/Bronze/4999. 아！/아！.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a = sc.next().length();
+		int b = sc.next().length();
+		if(a>=b){
+			System.out.println("go");
+		}else {
+			System.out.println("no");
+		}
+		
+	}
+}
