d426d1603b67d9e5761cfde8c8826b3446cf4df6
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-17 09:34:43 +0900

    [Bronze V] Title: 구구단, Time: 252 ms, Memory: 18584 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		for(int i=0; i<9; i++){
+			System.out.println(n+" * "+(i+1)+" = "+(n*(i+1)));
+		}
+		
+		
+	}
+}
