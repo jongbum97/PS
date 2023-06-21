50b57ad4c91d340d5f5c23263bfb042b5b56cfba
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-21 16:15:41 +0900

    [Bronze V] Title: 웰컴, Time: 200 ms, Memory: 17240 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println(".  .   .");
+		System.out.println("|  | _ | _. _ ._ _  _");
+		System.out.println("|/\\|(/.|(_.(_)[ | )(/.");
+		
+		
+	}
+	
+}
