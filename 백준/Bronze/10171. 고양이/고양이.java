c7073698cd08df4d0baf40508fb4b55f5a937734
jongbum97 <126944322+jongbum97@users.noreply.github.com>
2023-06-21 16:18:02 +0900

    [Bronze V] Title: 고양이, Time: 200 ms, Memory: 17476 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("\\    /\\");
+		System.out.println(" )  ( ')");
+		System.out.println("(  /  )");
+		System.out.println(" \\(__)|");
+		
+		
+	}
+	
+}
