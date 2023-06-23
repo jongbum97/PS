d8416a964e7cd544e5a051d0bf677413f8f9d8ec
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-06-23 09:08:57 +0900

    [Bronze V] Title: 마이크로소프트 로고, Time: 200 ms, Memory: 17036 KB -BaekjoonHub

---
+++ 백준/Bronze/5338. 마이크로소프트 로고/마이크로소프트 로고.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("       _.-;;-._");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-;;-._|");
+		System.out.println("'-..-'|   ||   |");
+		System.out.println("'-..-'|_.-''-._|");
+	}
+	
+}
