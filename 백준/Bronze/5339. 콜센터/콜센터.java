f9ff4dfe34ac97304d05d446ab74aa59f1f85356
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-06-23 09:10:24 +0900

    [Bronze V] Title: 콜센터, Time: 184 ms, Memory: 17360 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.java
@@ -0,0 +1,21 @@
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		String s = "     /~\\\n"
+				+ "    ( oo|\n"
+				+ "    _\\=/_\n"
+				+ "   /  _  \\\n"
+				+ "  //|/.\\|\\\\\n"
+				+ " ||  \\ /  ||\n"
+				+ "============\n"
+				+ "|          |\n"
+				+ "|          |\n"
+				+ "|          |";
+		System.out.println(s);
+	}
+	
+}
