2ec7709fa74c91613c48ee170a5a5031d6103268
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-06-25 19:28:18 +0900

    [Bronze V] Title: 홍익대학교, Time: 208 ms, Memory: 17536 KB -BaekjoonHub

---
+++ 백준/Bronze/16394. 홍익대학교/홍익대학교.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.*;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		System.out.println(n-1946);
+		
+	}
+	
+}
