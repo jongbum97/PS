0bc9a8936fdb735a67b249d2a24091734b724360
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-07-12 17:42:02 +0900

    [Bronze III] Title: 별 찍기 - 5, Time: 228 ms, Memory: 17848 KB -BaekjoonHub

---
+++ 백준/Bronze/2442. 별 찍기 － 5/별 찍기 － 5.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j < n-i-1; j++) {
+				sb.append(" ");
+			}
+			for (int j = 1; j <= 2*i+1; j++) {
+				sb.append("*");
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb);
+		
+		
+	}
+}
