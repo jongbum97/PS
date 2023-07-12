0e735452ff7e738acf2a1cb705878689e6b6417b
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-07-12 17:05:53 +0900

    [Bronze V] Title: 별 찍기 - 1, Time: 304 ms, Memory: 18064 KB -BaekjoonHub

--- 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
+++ 백준/Bronze/2438. 별 찍기 － 1/별 찍기 － 1.java
@@ -2,18 +2,18 @@ import java.util.*;
 
 public class Main {
 	public static void main(String[] args) {
-
+		
 		Scanner sc = new Scanner(System.in);
-
-			int N = sc.nextInt();
-			
-			for(int i=0; i<N; i++){
-				for(int j=N-i-1; j<N ; j++){
-					System.out.print("*");
-				}
-				System.out.println();
+		
+		int n = sc.nextInt();
+		for (int i = 0; i < n; i++) {
+			for (int j = 0; j <= i; j++) {
+				System.out.print("*");
 			}
-			
-			
+			System.out.println();
+		}
+		
+		
+		
 	}
 }
