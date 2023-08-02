da1fed29686ef6ca9c5d3012d76ea77c0b9d300b
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-08-02 17:52:02 +0900

    [D3] Title: 새샘이의 7-3-5 게임, Time: 255 ms, Memory: 39,048 KB -BaekjoonHub

---
+++ SWEA/D3/5948. 새샘이의 7－3－5 게임/새샘이의 7－3－5 게임.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Solution {
+
+	static byte nums[], t;
+	static int ans[];
+
+	public static void main(String[] args) throws Exception {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int T = Integer.parseInt(br.readLine());
+		
+		
+		for (int tc = 1; tc <= T; tc++) {
+			t = 0;
+			nums = new byte[7];
+			ans = new int[35];
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			for (int i = 0; i < 7; i++) {
+				nums[i] = Byte.parseByte(st.nextToken());
+			}
+			
+			a(0, 0, 0);
+			Arrays.sort(ans);
+			int k = 0;
+			for (int i = 34; i >=0 ; i--) {
+				if (ans[i] != ans[i-1]) {
+					k++;
+				}
+				if (k==4) {
+					System.out.println("#"+tc+" "+ans[i-1]);
+					break;
+				}
+			}
+		}
+	}
+	
+	
+	static void a(int n, int m, int sum) {
+		
+		if (n==3) {
+			ans[t++] = sum;
+			return;
+		}
+		
+		for (int i = m; i < 7; i++) {
+			a(n+1, i+1, sum+nums[i]);
+		}
+	}
+	
+
+
+}
