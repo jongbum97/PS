fd4ef12497861829b31ffca5fa7fd369e6979972
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2023-08-21 16:09:52 +0900

    [Gold III] Title: 줄 세우기, Time: 464 ms, Memory: 48852 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -3,79 +3,54 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static List<Integer>[] list1, list2;
-	static List<Integer> roots;
-	static boolean visit[];
-	static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+	static int N, M, edgeCount[];
+	static List<Integer>[] list;
 
 	public static void main(String[] args) throws Exception {
 
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		list1 = new ArrayList[N+1];
-		list2 = new ArrayList[N+1];
+		list = new ArrayList[N+1];
 		for (int i = 1; i <= N; i++) {
-			list1[i] = new ArrayList<>();
-			list2[i] = new ArrayList<>();
+			list[i] = new ArrayList<>();
 		}
+		edgeCount = new int[N+1];
 		for (int i = 0; i < M; i++) {
 			st = new StringTokenizer(br.readLine());
 			int a = Integer.parseInt(st.nextToken());
 			int b = Integer.parseInt(st.nextToken());
-			list1[a].add(b);
-			list2[b].add(a);
+			list[a].add(b);
+			edgeCount[b]++;
 		}
 		
-		visit = new boolean[N+1];
-		roots = new ArrayList<>();
+		
+		Queue<Integer> q = new LinkedList<>();
 		for (int i = 1; i <= N; i++) {
-			if (!visit[i]) root(i);
+			if(edgeCount[i]==0) q.add(i);
 		}
 		
-		
-		visit = new boolean[N+1];
-		for (int e : roots) {
-			DFS(e);
+		while (!q.isEmpty()) {
+			
+			int n = q.remove();
+			
+			for (int e : list[n]) {
+				edgeCount[e]--;
+				if (edgeCount[e]==0) {
+					q.add(e);
+				}
+			}
+			
+			bw.write(n+" ");
+			
 		}
+		
 		
 		bw.close();
 		
 		
 	}
-	
-
-	
-	static void DFS(int x) throws IOException {
-		
-		visit[x] = true;
-		
-		for (int e : list2[x]) {
-			if (!visit[e]) {
-				DFS(e);
-			}
-		}
-		
-		bw.write(x+" ");
-		
-	}
-
-	static void root(int x) {
-		
-		visit[x] = true;
-		
-		if (list1[x].size()==0) {
-			roots.add(x);
-		}
-		
-		for (int e : list1[x]) {
-			if(visit[e]) continue;
-			root(e);
-		}
-		
-	}
-	
 	
 }
