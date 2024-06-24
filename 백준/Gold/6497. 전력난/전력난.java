9034c3d64e87300b5f85536e3c37ce50aeb3b99b
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-06-24 22:56:33 +0900

    [Gold IV] Title: 전력난, Time: 904 ms, Memory: 256900 KB -BaekjoonHub

--- 백준/Gold/6497. 전력난/전력난.java
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -3,8 +3,8 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M;
-	static List<Node>[] line;
+	static int N, M, parent[];
+	static List<Node> line;
 
 	public static void main(String[] args) throws Exception {
 
@@ -18,21 +18,36 @@ public class Main {
 			M = Integer.parseInt(st.nextToken());
 			if(N == 0 && M == 0) break;
 
-			line = new ArrayList[N];
-			for (int i=0; i<N; i++){
-				line[i] = new ArrayList<>();
-			}
+			line = new ArrayList<>();
 			for(int i=0; i<M; i++){
 				st = new StringTokenizer(br.readLine());
 				int a = Integer.parseInt(st.nextToken());
 				int b = Integer.parseInt(st.nextToken());
 				int c = Integer.parseInt(st.nextToken());
-				line[a].add(new Node(b, c));
-				line[b].add(new Node(a, c));
+				line.add(new Node(a, b, c));
 				sum += c;
 			}
 
-			sb.append(sum-prim()).append("\n");
+			line.sort((a,b) -> a.cost - b.cost);
+
+			parent = new int[N];
+			for(int i=0; i<N; i++){
+				parent[i] = i;
+			}
+			int ans = 0;
+			int cnt = 0;
+			for(Node n : line){
+				int a = find(n.x);
+				int b = find(n.y);
+				if(a == b) continue;
+
+				union(a, b);
+				ans += n.cost;
+				cnt++;
+				if(cnt == N-1) break;
+			}
+
+			sb.append(sum-ans).append("\n");
 
 		}
 
@@ -40,37 +55,25 @@ public class Main {
 
 	}
 
-	static int prim(){
-		boolean[] visit = new boolean[N];
-		int ans = 0;
-
-		Queue<Node> q = new PriorityQueue<>((a,b) -> a.cost - b.cost);
-		q.add(new Node(0, 0));
-
-		while (!q.isEmpty()){
-
-			Node n = q.remove();
-			if(visit[n.y]) continue;
-
-			visit[n.y] = true;
-			ans += n.cost;
-
-			for(Node nn : line[n.y]){
-				if(visit[nn.y]) continue;
-				q.add(nn);
-			}
-
-		}
-
-		return ans;
-
+	static void union(int x, int y){
+		x = find(x);
+		y = find(y);
+		parent[x] = y;
 	}
+
+	static int find(int n){
+		if(parent[n] == n) return n;
+		return parent[n] = find(parent[n]);
+	}
+
 
 }
 class Node{
+	int x;
 	int y;
 	int cost;
-	Node(int y, int cost){
+	Node(int x, int y, int cost){
+		this.x = x;
 		this.y = y;
 		this.cost = cost;
 	}
