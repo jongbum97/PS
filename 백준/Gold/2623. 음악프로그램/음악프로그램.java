39cad871396d0744a44e2e33256a1ab56d3986eb
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-07-01 12:48:44 +0900

    [Gold III] Title: 음악프로그램, Time: 80 ms, Memory: 12248 KB -BaekjoonHub

--- 백준/Gold/2623. 음악프로그램/음악프로그램.java
+++ 백준/Gold/2623. 음악프로그램/음악프로그램.java
@@ -3,7 +3,7 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M;
+	static int N, M, edgeCnt[];
 	static List<Integer>[] line;
 
 	public static void main(String[] args) throws Exception {
@@ -13,28 +13,26 @@ public class Main {
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
 		line = new ArrayList[N+1];
+		edgeCnt = new int[N+1];
 		for(int i=1; i<=N; i++){
 			line[i] = new ArrayList<>();
 		}
 		for(int i=0; i<M; i++){
 			st = new StringTokenizer(br.readLine());
 			int cnt = Integer.parseInt(st.nextToken());
-			int[] nums = new int[cnt];
-			for(int j=0; j<cnt; j++){
-				nums[j] = Integer.parseInt(st.nextToken());
-			}
-			for(int j=0; j<cnt; j++){
-				for(int k=j+1; k<cnt; k++){
-					line[nums[k]].add(nums[j]);
-				}
+			int a = Integer.parseInt(st.nextToken());
+			for(int j=1; j<cnt; j++){
+				int b = Integer.parseInt(st.nextToken());
+				line[a].add(b);
+				edgeCnt[b]++;
+				a = b;
 			}
 		}
 
 		Queue<Integer> q = new LinkedList<>();
 		for(int i=1; i<=N; i++){
-			if(line[i].isEmpty()){
+			if(edgeCnt[i] == 0){
 				q.add(i);
-				line[i].add(-1);
 			}
 		}
 
@@ -43,25 +41,20 @@ public class Main {
 
 			int n = q.remove();
 
-			for(int i=1; i<=N; i++){
-				for(int j=line[i].size()-1; j>=0; j--){
-					if(line[i].get(j).equals(n)) {
-						line[i].remove(j);
-					}
-				}
-				if(line[i].isEmpty()){
-					q.add(i);
-					line[i].add(-1);
+			for(int nn : line[n]){
+				if(--edgeCnt[nn] == 0){
+					q.add(nn);
 				}
 			}
+
 			sb.append(n).append("\n");
 			cnt++;
 
 		}
+		
 		if(cnt != N){
 			sb = new StringBuilder("0\n");
 		}
-
 		System.out.println(sb);
 
 	}
