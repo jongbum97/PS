b5f376c7c40388199b6a3e07d00e085584c06ed0
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-07-02 12:45:36 +0900

    [Gold III] Title: 앱, Time: 112 ms, Memory: 16632 KB -BaekjoonHub

--- 백준/Gold/7579. 앱/앱.java
+++ 백준/Gold/7579. 앱/앱.java
@@ -15,19 +15,16 @@ public class Main {
 		cost = new int[N+1];
 		dp = new int[N+1][10_001];
 		st = new StringTokenizer(br.readLine());
+		StringTokenizer st2 = new StringTokenizer(br.readLine());
 		for(int i=1; i<=N; i++){
-			memory[i] = Integer.parseInt(st.nextToken());
-		}
-		st = new StringTokenizer(br.readLine());
-		for(int i=1; i<=N; i++){
-			cost[i] = Integer.parseInt(st.nextToken());
-			dp[i][cost[i]] = memory[i];
+			dp[i][cost[i] = Integer.parseInt(st2.nextToken())] = memory[i] = Integer.parseInt(st.nextToken());
 		}
 
 		for(int i=1; i<=N; i++){
-			dp[i][0] = Math.max(dp[i][0], dp[i-1][0]);
 			if(cost[i] == 0){
 				dp[i][0] = Math.max(dp[i][0], dp[i-1][0] + memory[i]);
+			}else{
+				dp[i][0] = Math.max(dp[i][0], dp[i-1][0]);
 			}
 			for(int j=1; j<10001; j++){
 				dp[i][j] = Math.max(dp[i][j], Math.max(dp[i-1][j], dp[i][j-1]));
@@ -37,7 +34,7 @@ public class Main {
 			}
 		}
 
-		for(int i=0; i<=10001; i++){
+		for(int i=0; i<10001; i++){
 			if(dp[N][i] >= M){
 				System.out.println(i);
 				return;
