e4ff684d6b16e136233b81720fc678abb9267bd6
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-07-04 15:35:37 +0900

    [Gold III] Title: 두 배열의 합, Time: 340 ms, Memory: 60276 KB -BaekjoonHub

--- 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
+++ 백준/Gold/2143. 두 배열의 합/두 배열의 합.java
@@ -31,25 +31,16 @@ public class Main {
 				sA[cnt++] = A[j] - A[i];
 			}
 		}
+
 		sB = new HashMap<>();
-		cnt = 0;
 		for(int i=0; i<=M; i++){
 			for(int j=i+1; j<=M; j++){
-				int t = B[j] - B[i];
-				if(sB.get(t) == null){
-					sB.put(t, 1);
-				}else{
-					sB.put(t, sB.get(t)+1);
-				}
-
+				sB.put(B[j] - B[i], sB.getOrDefault(B[j] - B[i], 0) + 1);
 			}
 		}
 
-		for(int i=0; i<N*(N+1)/2; i++){
-			int tmp = T - sA[i];
-			if(sB.get(tmp) != null){
-				ans += sB.get(tmp);
-			}
+		for(int n : sA){
+			ans += sB.getOrDefault(T - n, 0);
 		}
 
 		System.out.println(ans);
