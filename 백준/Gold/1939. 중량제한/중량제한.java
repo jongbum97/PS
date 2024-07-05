661ed1fd338f57a60a2be6193c584aee4a8e1680
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-07-05 10:19:55 +0900

    [Gold III] Title: 중량제한, Time: 384 ms, Memory: 48816 KB -BaekjoonHub

--- 백준/Gold/1939. 중량제한/중량제한.java
+++ 백준/Gold/1939. 중량제한/중량제한.java
@@ -3,9 +3,8 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M, start, end, dis[];
-	static Map<Integer, Integer> line[];
-	static boolean visit[];
+	static int N, M, start, end, parent[];
+	static Queue<Node> q;
 
 	public static void main(String[] args) throws Exception {
 
@@ -13,51 +12,47 @@ public class Main {
 		StringTokenizer st = new StringTokenizer(br.readLine());
 		N = Integer.parseInt(st.nextToken());
 		M = Integer.parseInt(st.nextToken());
-		line = new HashMap[N+1];
-		for(int i=1; i<=N; i++){
-			line[i] = new HashMap<>();
-		}
-		while(M-- > 0){
+		q = new PriorityQueue<>();
+		while(--M >= 0){
 			st = new StringTokenizer(br.readLine());
-			int a = Integer.parseInt(st.nextToken());
-			int b = Integer.parseInt(st.nextToken());
-			int c = Integer.parseInt(st.nextToken());
-			line[a].put(b, Math.max(line[a].getOrDefault(b, 0), c));
-			line[b].put(a, Math.max(line[b].getOrDefault(a, 0), c));
+			q.add(new Node(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken())));
 		}
 		st = new StringTokenizer(br.readLine());
 		start = Integer.parseInt(st.nextToken());
 		end = Integer.parseInt(st.nextToken());
 
+		parent = new int[N+1];
+		for(int i=1; i<N; i++){
+			parent[i] = i;
+		}
 
-		dis = new int[N+1];
-		Arrays.fill(dis, Integer.MAX_VALUE);
-		visit = new boolean[N+1];
-		Queue<Node> q = new PriorityQueue<>();
-		q.add(new Node(0, start, Integer.MAX_VALUE));
+		int ans = Integer.MAX_VALUE;
+		while(find(start) != find(end)){
 
-		while(!q.isEmpty()){
 			Node n = q.remove();
-
-			if(visit[n.y]) continue;
-			visit[n.y] = true;
-			dis[n.y] = Math.min(dis[n.x], n.cost);
-
-			if(n.y == end) break;
-
-			line[n.y].forEach((y, cost) -> {
-				if(!visit[y]){
-					q.add(new Node(n.y, y, cost));
-				}
-			});
+			if(find(n.x) != find(n.y)){
+				union(n.x, n.y);
+				ans = Math.min(ans, n.cost);
+			}
 
 		}
 
-		System.out.println(dis[end]);
+		System.out.println(ans);
 
 	}
 
-	static class Node implements Comparable{
+	static void union(int a, int b){
+		a = find(a);
+		b = find(b);
+		parent[a] = b;
+	}
+
+	static int find(int n){
+		if(parent[n] == n) return n;
+		return parent[n] = find(parent[n]);
+	}
+
+	static class Node implements Comparable<Node>{
 		int x;
 		int y;
 		int cost;
@@ -68,9 +63,12 @@ public class Main {
 		}
 
 		@Override
-		public int compareTo(Object o){
-			return ((Node)o).cost - this.cost;
+		public int compareTo(Node o){
+			return o.cost - this.cost;
 		}
 	}
+
+
+
 
 }
