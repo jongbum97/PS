f24a528e6025212ca6333551318e925f35a0df33
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-07-14 16:05:24 +0900

    [Gold II] Title: 게임, Time: 116 ms, Memory: 15772 KB -BaekjoonHub

--- 백준/Gold/1103. 게임/게임.java
+++ 백준/Gold/1103. 게임/게임.java
@@ -25,46 +25,34 @@ public class Main {
 		for(int i=0; i<N; i++){
 			Arrays.fill(dp[i], -1);
 		}
-		dp[0][0] = 1;
-		for(int i=0; i<M*N; i++){
-			bellmanFord();
-		}
-
-		int[][] tmp = new int[N][];
-		for(int i=0; i<N; i++){
-			tmp[i] = dp[i].clone();
-		}
-		bellmanFord();
-
-		for(int i=0; i<N; i++){
-			for(int j=0; j<M; j++){
-				if(tmp[i][j] != dp[i][j]){
-					System.out.println(-1);
-					return;
-				}
-				ans = Math.max(ans, dp[i][j]);
-			}
-		}
+		dfs(0, 0, 1);
 
 		System.out.println(ans);
 
 	}
 
-	static void bellmanFord(){
+	static void dfs(int x, int y, int cnt){
 
-		for(int i=0; i<N; i++){
-			for(int j=0; j<M; j++){
-				if(dp[i][j] == -1) continue;
-				for(int k=0; k<4; k++){
-					int a = i + dr[k]*map[i][j];
-					int b = j + dc[k]*map[i][j];
-					if(a<0 || a>N-1 || b<0 || b>M-1) continue;
-					if(map[a][b] == 24) continue;
-					dp[a][b] = Math.max(dp[a][b], dp[i][j] + 1);
-				}
-			}
+		if(visit[x][y] || ans == -1){
+			ans = -1;
+			return;
+		}
+		visit[x][y] = true;
+		dp[x][y] = cnt;
+
+		for(int i=0; i<4; i++){
+			int a = x + dr[i]*map[x][y];
+			int b = y + dc[i]*map[x][y];
+			if(a<0 || a>N-1 || b<0 || b> M-1) continue;
+			if(map[a][b] == 24) continue;
+			if(dp[a][b] > cnt) continue;
+			dfs(a, b, cnt+1);
 		}
 
+		visit[x][y] = false;
+		if(ans != -1){
+			ans = Math.max(ans, cnt);
+		}
 	}
 
 }
