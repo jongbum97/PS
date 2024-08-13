84bf61c93f2a4b04e48725b5fa051a7dd2ef837b
JongBum Kim <126944322+jongbum97@users.noreply.github.com>
2024-08-13 11:38:38 +0900

    [Gold III] Title: 벽 부수고 이동하기 2, Time: 1340 ms, Memory: 335560 KB -BaekjoonHub

--- 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
+++ 백준/Gold/14442. 벽 부수고 이동하기 2/벽 부수고 이동하기 2.java
@@ -3,7 +3,8 @@ import java.util.*;
 
 public class Main {
 
-	static int N, M, K, map[][], dr[] = {-1, 0, 1, 0}, dc[] = {0, 1, 0, -1}, dist[][][];
+	static int N, M, K, map[][], dr[] = {-1, 0, 1, 0}, dc[] = {0, 1, 0, -1}, ans;
+	static boolean visit[][][];
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -18,46 +19,44 @@ public class Main {
 				map[i][j] = input.charAt(j) - '0';
 			}
 		}
-		dist = new int[N][M][K+1];
-		for(int i=0; i<N; i++){
-			for(int j=0; j<M; j++){
-				Arrays.fill(dist[i][j], Integer.MAX_VALUE);
-			}
-		}
+		visit = new boolean[N][M][K+1];
+		ans = Integer.MAX_VALUE;
 
 		Queue<Node> q = new ArrayDeque<>();
-		q.add(new Node(0, 0, 0, dist[0][0][0] = 0));
+		q.add(new Node(0, 0, 0, 1));
+		visit[0][0][0] = true;
 
 		while(!q.isEmpty()){
 
 			Node n = q.remove();
-
-			if(dist[n.x][n.y][n.cnt] < n.dist) continue;
+			if(n.x==N-1 && n.y==M-1) {
+				ans = Math.min(ans, n.dist);
+				continue;
+			}
 
 			for(int i=0; i<4; i++){
 				int a = n.x + dr[i];
 				int b = n.y + dc[i];
-				if(a<0 || a>N-1 || b<0 || b>M-1) continue;
+				if(a<0 || a>N-1 || b<0 || b>M-1 ) continue;
 				if(map[a][b] == 0){
-					if(dist[a][b][n.cnt] > n.dist+1)
-						q.add(new Node(a, b, n.cnt, dist[a][b][n.cnt] = n.dist+1));
-
+					if(!visit[a][b][n.k]){
+						q.add(new Node(a, b, n.k, n.dist+1));
+						visit[a][b][n.k] = true;
+					}
 				}else{
-					if(n.cnt < K && dist[a][b][n.cnt+1] > n.dist+1)
-						q.add(new Node(a, b, n.cnt+1, dist[a][b][n.cnt+1] = n.dist+1));
+					if(n.k < K && !visit[a][b][n.k+1]){
+						q.add(new Node(a, b, n.k+1, n.dist+1));
+						visit[a][b][n.k+1] = true;
+					}
 				}
 			}
 
 		}
 
-		int ans = Integer.MAX_VALUE;
-		for(int i=0; i<=K; i++){
-			ans = Math.min(ans, dist[N-1][M-1][i]);
-		}
 		if(ans == Integer.MAX_VALUE)
-			ans = -2;
+			ans = -1;
 
-		System.out.println(ans+1);
+		System.out.println(ans);
 
 	}
 
@@ -65,11 +64,11 @@ public class Main {
 		int x;
 		int y;
 		int dist;
-		int cnt;
-		Node(int x, int y, int cnt, int dist){
+		int k;
+		Node(int x, int y, int k, int dist){
 			this.x = x;
 			this.y = y;
-			this.cnt = cnt;
+			this.k = k;
 			this.dist = dist;
 		}
 	}
